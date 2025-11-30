c800d546e84137e54a505c62b91bc8be1e48fd88
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-11-30 11:22:15 -0300

    Add files via upload

---
+++ ApiTransferencia/src/main/java/MuriloWisch/Dev/ApiTransferencia/ApiTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package MuriloWisch.Dev.ApiTransferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransferenciaApplication.class, args);
+	}
+
+}
---
+++ ApiTransferencia/src/test/java/MuriloWisch/Dev/ApiTransferencia/ApiTransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package MuriloWisch.Dev.ApiTransferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
