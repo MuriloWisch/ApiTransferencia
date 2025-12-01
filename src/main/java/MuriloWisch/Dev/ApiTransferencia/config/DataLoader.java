e2c23a304c8223a828b600e68d9ce097da7dd3a9
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-01 17:10:32 -0300

    add dataloader

---
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/config/DataLoader.java
@@ -0,0 +1,15 @@
+package MuriloWisch.Dev.ApiTransferencia.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
