6e9d44b7952ed5aa7b7e980f3438a7450db10f52
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-10 11:29:03 -0300

    add datas validator

--- src/main/java/MuriloWisch/Dev/ApiTransferencia/controller/RestExceptionHandler.java
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/controller/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package MuriloWisch.Dev.ApiTransferencia.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,7 +18,16 @@ public class RestExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
 
+        var fieldErrors = e.getFieldErrors().stream().map(f -> new InvalidParam(f.getField(),f.getDefaultMessage())).toList();
 
-        return null;
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        pb.setTitle("O seu paramatro não é válido");
+        pb.setProperty("Parametros invalidos", fieldErrors);
+
+        return pb;
+    }
+    private record InvalidParam(String name, String reason){
+
     }
 }
