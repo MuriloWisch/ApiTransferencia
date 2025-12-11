efd47b56a60298a22b72e180c4751bedfb767af8
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-10 23:17:22 -0300

    Att: att CarteiraNotFoundException

--- src/main/java/MuriloWisch/Dev/ApiTransferencia/exception/CarteiraNotFoundException.java
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/exception/CarteiraNotFoundException.java
@@ -1,7 +1,23 @@
 package MuriloWisch.Dev.ApiTransferencia.exception;
 
-public class CarteiraNotFoundException extends RuntimeException {
-  public CarteiraNotFoundException(String message) {
-    super(message);
-  }
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class CarteiraNotFoundException extends TransferenciaException {
+
+    private Long carteiraId;
+
+    public CarteiraNotFoundException(Long carteiraId) {
+        this.carteiraId = carteiraId;
+    }
+
+    /*Aqui neste trecho é onde detalhamos oque sera retornado no erro*/
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var pb = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        pb.setTitle("Carteira não encontrada");
+        pb.setDetail("Não existe carteira com o id" + carteiraId + ".");
+    }
 }
