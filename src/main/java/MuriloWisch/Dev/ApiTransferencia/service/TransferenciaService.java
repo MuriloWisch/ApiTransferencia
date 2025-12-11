60b65635e1eadb5c54573c10f034f31719950288
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-10 22:57:25 -0300

    Att: TransferenciaService

--- src/main/java/MuriloWisch/Dev/ApiTransferencia/service/TransferenciaService.java
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/service/TransferenciaService.java
@@ -1,7 +1,13 @@
 package MuriloWisch.Dev.ApiTransferencia.service;
 
+import MuriloWisch.Dev.ApiTransferencia.controller.dto.TransferenciaDto;
+import MuriloWisch.Dev.ApiTransferencia.entity.Transferencia;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransferenciaService {
+    public Transferencia transferencia(@Valid TransferenciaDto dto) {
+
+    }
 }
