28e2131d732cffeb728df77eb29b201de09f9779
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-23 09:49:47 -0300

    Add: atualizarSaldo method on CarteiraRepository

--- src/main/java/MuriloWisch/Dev/ApiTransferencia/repository/CarteiraRepository.java
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/repository/CarteiraRepository.java
@@ -1,10 +1,21 @@
 package MuriloWisch.Dev.ApiTransferencia.repository;
 
 import MuriloWisch.Dev.ApiTransferencia.entity.Carteira;
+import feign.Param;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface CarteiraRepository extends JpaRepository<Carteira, Long> {
     Optional findByCpfCnpjOrEmail(String cpfCnpj, String email);
+
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Carteira c SET c.saldo = :saldo WHERE c.id = :id")
+    void atualizarSaldo(@Param ("id") Long id, @Param("saldo")BigDecimal saldo);
 }
