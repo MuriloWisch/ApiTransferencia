c1449e0080c8da2561d43714fec700d87816005e
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-23 10:02:40 -0300

    Add: CarteiraService

--- src/main/java/MuriloWisch/Dev/ApiTransferencia/service/CarteiraService.java
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/service/CarteiraService.java
@@ -3,6 +3,7 @@ package MuriloWisch.Dev.ApiTransferencia.service;
 import MuriloWisch.Dev.ApiTransferencia.controller.dto.CriarCarteiraDto;
 import MuriloWisch.Dev.ApiTransferencia.entity.Carteira;
 import MuriloWisch.Dev.ApiTransferencia.exception.CarteiraDataAlreadyExistsException;
+import MuriloWisch.Dev.ApiTransferencia.exception.SaldoInsuficienteException;
 import MuriloWisch.Dev.ApiTransferencia.repository.CarteiraRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +22,16 @@ public class CarteiraService {
 
         var carteiraDb = carteiraRepository.findByCpfCnpjOrEmail(dto.cpfCnpj(),dto.email());
         if (carteiraDb.isPresent()){
-            throw new CarteiraDataAlreadyExistsException("CpfCnpj ou Email ja existe");
+            throw new CarteiraDataAlreadyExistsException("CpfCnpj ou Email ja existe.");
         }
         return carteiraRepository.save(dto.toCarteira());
     }
 
 
     public void atualizarSaldo(Long id, BigDecimal novoSaldo) {
+        if (novoSaldo.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Não é permitido transferir um valor menor que 0.");
+        }
+        carteiraRepository.atualizarSaldo(id,novoSaldo);
     }
 }
