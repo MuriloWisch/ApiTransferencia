7b1a251e3d9ef7abc1038218c9b1a1f353b0994d
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-12-23 09:44:05 -0300

    Att system

---
+++ src/main/java/MuriloWisch/Dev/ApiTransferencia/controller/dto/AtualizarSaldoDto.java
@@ -0,0 +1,15 @@
+package MuriloWisch.Dev.ApiTransferencia.controller.dto;
+
+import java.math.BigDecimal;
+
+public class AtualizarCarteiraDto {
+    private BigDecimal saldo;
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
